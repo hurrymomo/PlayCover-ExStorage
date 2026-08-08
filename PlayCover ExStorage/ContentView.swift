49534dd6df1a5d639d6f1eef944431f77d9e1588
momo <hurrymomo@hotmail.com>
2026-08-07 17:24:26 -0700

    Initial Commit

---
+++ PlayCover ExStorage/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlayCover ExStorage
+//
+//  Created by momo on 2026-08-07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlayCover ExStorage/PlayCover_ExStorageApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayCover_ExStorageApp.swift
+//  PlayCover ExStorage
+//
+//  Created by momo on 2026-08-07.
+//
+
+import SwiftUI
+
+@main
+struct PlayCover_ExStorageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
